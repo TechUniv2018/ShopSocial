010dd56c7cea5b78c49e2bfb159ac47537ca3118
maheshhp <hpmahesh73@gmail.com>
2018-02-14 22:55:17 +0530

    helper - Tests passing for getProductById function

--- src/tests/api/v1/products/getProductById.test.js
+++ src/tests/api/v1/products/getProductById.test.js
@@ -2,11 +2,23 @@ const getProductByID = require('../../../../routes/api/v1/products/getProductByI
 
 describe('Tests for getting product details by ID from external API', () => {
   test('Test for checking return result of getProductById function for valid product ID', (done) => {
-    expect(typeof getProductByID()).resolves.toEqual(false);
-    done();
+    getProductByID(9132294).then((result) => {
+      expect(result.upc).toMatch('086792895093');
+      done();
+    });
   });
   test('Test for checking return result of getProductById function for invalid product ID', (done) => {
-    expect(typeof getProductByID(1234)).resolves.toEqual(false);
-    done();
+    const responseObject = {
+      name: 'NotFound',
+      message: "No record found for id '1234'",
+      code: 404,
+      className: 'not-found',
+      errors: {},
+    };
+    getProductByID(1234).then()
+      .catch((error) => {
+        expect(error.error).toEqual(responseObject);
+        done();
+      });
   });
 });
