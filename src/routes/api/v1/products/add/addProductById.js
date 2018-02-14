8b9a9de548e935f2b98d9e2c67c02133cd5b3a4a
maheshhp <hpmahesh73@gmail.com>
2018-02-14 23:26:10 +0530

    route - Added test for successful insert of product data into DB

--- src/routes/api/v1/products/add/addProductById.js
+++ src/routes/api/v1/products/add/addProductById.js
@@ -1,11 +1,21 @@
+const getProductByID = require('../getProductById');
+const Models = require('../../../../../../models/');
+
 module.exports = [
   {
     method: 'GET',
     path: '/api/v1/products/add/{productId}',
     handler: (request, response) => {
-      response({
-        action: 'Product added',
-        statusCode: 201,
+      getProductByID(request.params.productId).then((result) => {
+        response({
+          action: 'Product added',
+          statusCode: 201,
+        });
+      }).catch((error) => {
+        response({
+          action: error.error.name,
+          statusCode: error.error.code,
+        });
       });
     },
   },
--- src/tests/api/v1/products/add/addProductById.test.js
+++ src/tests/api/v1/products/add/addProductById.test.js
@@ -1,10 +1,26 @@
 const server = require('../../../../../server');
+const Models = require('../../../../../../models/');
+
+beforeEach(() => Models.ProductDetails.destroy({ truncate: true }));
+afterAll(() => Models.close());
+
+const sampleProductDBObject = {
+  productID: 43900,
+  name: 'Duracell - AAA Batteries (4-Pack)',
+  price: 5.49,
+  upc: '041333424019',
+  description: 'Compatible with select electronic devices; AAA size; DURALOCK Power Preserve technology; 4-pack',
+  manufacturer: 'Duracell',
+  model: 'MN2400B4Z',
+  image: 'http://img.bbystatic.com/BestBuy_US/images/products/4390/43900_sa.jpg',
+  category: 'Alkaline Batteries',
+};
 
 describe('Tests for adding product/s to the database from external API', () => {
   test('Test for successful response code from the server on a valid request', (done) => {
     const request = {
       method: 'GET',
-      url: '/api/v1/products/add/{9132294}',
+      url: '/api/v1/products/add/9132294',
     };
     server.inject(request, (response) => {
       expect(response.result.statusCode).toBe(201);
@@ -14,7 +30,7 @@ describe('Tests for adding product/s to the database from external API', () => {
   test('Test for successful response message from the server on a valid request', (done) => {
     const request = {
       method: 'GET',
-      url: '/api/v1/products/add/{9132294}',
+      url: '/api/v1/products/add/9132294',
     };
     server.inject(request, (response) => {
       expect(response.result.action).toMatch('Product added');
@@ -24,11 +40,32 @@ describe('Tests for adding product/s to the database from external API', () => {
   test('Test for successful response message from the server on a invalid request', (done) => {
     const request = {
       method: 'GET',
-      url: '/api/v1/products/add/{1234}',
+      url: '/api/v1/products/add/1234',
     };
     server.inject(request, (response) => {
-      expect(response.result.action).toMatch('Invalid product');
+      expect(response.result.action).toMatch('NotFound');
       done();
+    });
+  });
+  test('Test for successful insert into database on a valid request', (done) => {
+    const request = {
+      method: 'GET',
+      url: '/api/v1/products/add/43900',
+    };
+    server.inject(request, (response) => {
+      Models.ProductDetails.find().then((result) => {
+        expect(result.productID).toBe(sampleProductDBObject.productID);
+        expect(result.name).toBe(sampleProductDBObject.name);
+        expect(result.price).toBe(sampleProductDBObject.price);
+        expect(result.upc).toBe(sampleProductDBObject.upc);
+        expect(result.description).toBe(sampleProductDBObject.description);
+        expect(result.manufacturer).toBe(sampleProductDBObject.manufacturer);
+        expect(result.model).toBe(sampleProductDBObject.model);
+        expect(result.model).toBe(sampleProductDBObject.model);
+        expect(result.image).toBe(sampleProductDBObject.image);
+        expect(result.category).toBe(sampleProductDBObject.category);
+        done();
+      });
     });
   });
 });
