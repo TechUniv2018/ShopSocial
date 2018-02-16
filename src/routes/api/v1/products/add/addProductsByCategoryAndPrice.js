920f9c3ecc9a1858ea130d65696d755a1fbd6b06
maheshhp <hpmahesh73@gmail.com>
2018-02-17 00:27:57 +0530

    handler - Implemented handler for addProductsByCategoryAndPrice route

--- src/routes/api/v1/products/add/addProductsByCategoryAndPrice.js
+++ src/routes/api/v1/products/add/addProductsByCategoryAndPrice.js
@@ -8,11 +8,14 @@ module.exports = [
     handler: (request, response) => {
       getProductsByCategoryAndPrice(request.payload).then((remoteFetchResponse) => {
         addProductsToDatabase(remoteFetchResponse.data).then((dbInsertResponse) => {
-          console.log(dbInsertResponse);
+          let addedIDs = '';
+          dbInsertResponse.forEach((insertedValue) => {
+            addedIDs += `${insertedValue.dataValues.productID};`;
+          });
           response({
             statusCode: 201,
             action: 'Product/s added',
-            added: ';',
+            added: addedIDs,
           });
         });
       }).catch(() => {
