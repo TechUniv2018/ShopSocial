fba96f0f903a8f96954bffc2e6929970a3ee0ab4
Aakash Verma <aku06_ran@yahoo.co.in>
2018-02-18 12:21:29 +0530

    OrderDetails - Added test for multiple orders

--- __tests__/models/OrderDetails.test.js
+++ __tests__/models/OrderDetails.test.js
@@ -65,4 +65,28 @@ describe('Testing the OrderDetails table', () => {
       });
     });
   });
+
+  it('testing with 2 sample valid order objects', (done) => {
+    const sampleOrder = {
+      orderID: 'OD1234',
+      userIDs: ['user1'],
+      productIDs: [123, 456],
+      productPrices: [1.23, 4.56],
+      orderStatus: 'CANCELLED',
+    };
+
+    const sampleOrder2 = {
+      orderID: 'OD5678',
+      userIDs: ['user1', 'user2'],
+      productIDs: [123, 456],
+      productPrices: [1.23, 4.56],
+      orderStatus: 'ONGOING',
+    };
+    Models.OrderDetails.bulkCreate([sampleOrder, sampleOrder2]).then(() => {
+      Models.OrderDetails.findAll({ attributes: ['orderID'] }).then((result) => {
+        expect(result.sort().map(a => a.orderID)).toEqual(['OD1234', 'OD5678'].sort());
+        done();
+      });
+    });
+  });
 });
