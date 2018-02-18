64925465b862ba6ffaff06375451cff97b28ebfe
Aakash Verma <aku06_ran@yahoo.co.in>
2018-02-18 12:10:02 +0530

    OrderDetails - Added test to check all ENUM values are working by checking "CANCELLED"

--- __tests__/models/OrderDetails.test.js
+++ __tests__/models/OrderDetails.test.js
@@ -45,4 +45,24 @@ describe('Testing the OrderDetails table', () => {
       });
     });
   });
+  it('testing with sample valid order object for orderStatus "CANCELLED" ', (done) => {
+    const sampleOrder = {
+      orderID: 'OD1234',
+      userIDs: ['user1'],
+      productIDs: [123, 456],
+      productPrices: [1.23, 4.56],
+      orderStatus: 'CANCELLED',
+    };
+
+    Models.OrderDetails.create(sampleOrder).then(() => {
+      Models.OrderDetails.find().then((result) => {
+        expect(result.orderID).toBe(sampleOrder.orderID);
+        expect(result.userIDs.sort()).toEqual(sampleOrder.userIDs.sort());
+        expect(result.productIDs.sort()).toEqual(sampleOrder.productIDs.sort());
+        expect(result.productPrices.sort()).toEqual(sampleOrder.productPrices.sort());
+        expect(result.orderStatus).toBe(sampleOrder.orderStatus);
+        done();
+      });
+    });
+  });
 });
