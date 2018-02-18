86904f2a69af744145eceb51158ec21d3a3801bd
Aakash Verma <aku06_ran@yahoo.co.in>
2018-02-18 10:30:04 +0530

    OrderDetails - Migrations and Models working.

---
+++ models/orderdetails.js
@@ -0,0 +1,17 @@
+
+module.exports = (sequelize, DataTypes) => {
+  const OrderDetails = sequelize.define('OrderDetails', {
+    OrderID: DataTypes.STRING,
+    UserIDs: DataTypes.ARRAY(DataTypes.STRING),
+    ProductIDs: DataTypes.ARRAY(DataTypes.INTEGER),
+    ProductPrices: DataTypes.ARRAY(DataTypes.FLOAT),
+    OrderStatus: DataTypes.ENUM('FULFILLED', 'CANCELLED', 'ONGOING'),
+  }, {
+    classMethods: {
+      associate(models) {
+        // associations can be defined here
+      },
+    },
+  });
+  return OrderDetails;
+};
