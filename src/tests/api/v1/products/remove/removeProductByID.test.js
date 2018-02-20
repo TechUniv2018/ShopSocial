a4cea75c1968ecfaa78496c99a4d424985ec46a7
Aakash Verma <aku06_ran@yahoo.co.in>
2018-02-20 15:05:03 +0530

    Added request method test

--- src/tests/api/v1/products/remove/removeProductByID.test.js
+++ src/tests/api/v1/products/remove/removeProductByID.test.js
@@ -1,13 +1,33 @@
 const Server = require('../../../../../server');
 
 describe('Testing route removeProductById', () => {
-  it('Testing the ID provided is a number', (done) => {
+  it('Testing the validity of route with ID', (done) => {
     const request = {
       method: 'DELETE',
       url: '/api/v1/products/remove/',
     };
     Server.inject(request, (response) => {
       expect(response.statusCode).toBe(400);
+      done();
+    });
+  });
+  it('Testing the method is DELETE only', (done) => {
+    let inSeries = false;
+    const requestGET = {
+      method: 'GET',
+      url: '/api/v1/products/remove/1234',
+    };
+    Server.inject(requestGET, (response) => {
+      expect(response.statusCode).toBe(404);
+      inSeries = true;
+    });
+    const requestPOST = {
+      method: 'GET',
+      url: '/api/v1/products/remove/1234',
+    };
+    Server.inject(requestPOST, (response) => {
+      expect(response.statusCode).toBe(404);
+      expect(inSeries).toBe(true);
       done();
     });
   });
