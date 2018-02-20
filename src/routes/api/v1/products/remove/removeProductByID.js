9dd14cd361c63ad4f9e7482647a275dc46d055da
Aakash Verma <aku06_ran@yahoo.co.in>
2018-02-20 16:10:09 +0530

    removeProductByID.test.js - Added test for deleting an item stored correctly

--- src/routes/api/v1/products/remove/removeProductByID.js
+++ src/routes/api/v1/products/remove/removeProductByID.js
@@ -1,3 +1,4 @@
+const Models = require('../../../../../../models/');
 const Joi = require('joi');
 
 module.exports = [
@@ -13,7 +14,30 @@ module.exports = [
       },
     },
     handler: (request, response) => {
-      response({ statusCode: 200 });
+      Models.ProductDetails.findOne({
+        where: {
+          productID: request.params.productId,
+        },
+      }).then((result) => {
+        if (result === null) {
+          throw new Error(`No Product with product id - ${request.params.productId} found in the db`);
+        }
+        Models.ProductDetails.destroy({
+          where: {
+            productID: request.params.productId,
+          },
+        }).then(() => {
+          response({
+            message: 'Product removed',
+            statusCode: 200,
+          });
+        });
+      }).catch((error) => {
+        response({
+          message: error.error.name,
+          statusCode: 204,
+        });
+      });
     },
   },
 ];
