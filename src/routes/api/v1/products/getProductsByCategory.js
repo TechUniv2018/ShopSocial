536a7f30e447bd03e9a7439598398ab054b499bc
maheshhp <hpmahesh73@gmail.com>
2018-02-20 08:53:11 +0530

    route - Implemented and tested getProductsByCategory route

--- src/routes/api/v1/products/getProductsByCategory.js
+++ src/routes/api/v1/products/getProductsByCategory.js
@@ -1,9 +1,50 @@
+const Models = require('../../../../../models/');
+
+const queryResultDecomposer = queryResult => ({
+  productID: queryResult.productID,
+  price: queryResult.price,
+  name: queryResult.name,
+  description: queryResult.description,
+  manufacturer: queryResult.manufacturer,
+  model: queryResult.model,
+  category: queryResult.category,
+  image: queryResult.image,
+  upc: queryResult.upc,
+});
+
 module.exports = [
   {
     method: 'GET',
     path: '/api/v1/products/category/{productCategory}',
     handler: (request, response) => {
-      response(true);
+      Models.ProductDetails.findAll({
+        where: {
+          category: request.params.productCategory,
+        },
+      }).then((queryResult) => {
+        console.log(queryResult);
+        if (queryResult.length === 0) {
+          response({
+            statusCode: 404,
+            error: 'Product/s not found',
+          });
+        } else {
+          const decomposedQueryResult = [];
+          queryResult.forEach((productObject) => {
+            decomposedQueryResult.push(queryResultDecomposer(productObject));
+          });
+          response({
+            statusCode: 200,
+            data: decomposedQueryResult,
+          });
+        }
+      }).catch((queryError) => {
+        console.log(queryError);
+        response({
+          statusCode: 404,
+          error: 'Error',
+        });
+      });
     },
   },
 ];
