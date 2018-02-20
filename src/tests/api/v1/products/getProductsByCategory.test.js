4ad59d78a9e31d7a7ccdbfba491e12ed0609494d
maheshhp <hpmahesh73@gmail.com>
2018-02-20 08:53:11 +0530

    route - Implemented and tested getProductsByCategory route

--- src/tests/api/v1/products/getProductsByCategory.test.js
+++ src/tests/api/v1/products/getProductsByCategory.test.js
@@ -1,6 +1,40 @@
 const server = require('../../../../server');
 const Models = require('../../../../../models/');
 
+const productsInDb = [{
+  productID: 1213167,
+  price: 199.99,
+  name: 'Insignia™ - 39" Class (38.5" Diag.) - LED - 720p - HDTV - Black',
+  description: '720p resolution60Hz refresh rate',
+  manufacturer: 'Insignia™',
+  model: 'NS-39D220NA16',
+  category: 'TV & Home Theater',
+  image: 'http://img.bbystatic.com/BestBuy_US/images/products/1213/1213167_sa.jpg',
+  upc: '600603185199',
+},
+{
+  productID: 4807511,
+  price: 199.99,
+  name: 'Insignia™ - 39" Class (38.5" Diag.) - LED - 720p - HDTV - Black',
+  description: '720p resolution60Hz refresh rate',
+  manufacturer: 'Insignia™',
+  model: 'NS-39D310NA17',
+  category: 'TV & Home Theater',
+  image: 'http://img.bbystatic.com/BestBuy_US/images/products/4807/4807511_sa.jpg',
+  upc: '600603198403',
+},
+{
+  productID: 4822001,
+  price: 169.99,
+  name: 'Insignia™ - 32" Class (31.5" Diag.) - LED - 720p - Smart - Roku TV - Black',
+  description: '720p resolution; 60Hz refresh rate; Roku TV',
+  manufacturer: 'Insignia™',
+  model: 'NS-32DR310NA17',
+  category: 'TV & Home Theater',
+  image: 'http://img.bbystatic.com/BestBuy_US/images/products/4822/4822001_sa.jpg',
+  upc: '600603198120',
+}];
+
 beforeEach((done) => {
   Models.ProductDetails.destroy({ truncate: true }).then(() => {
     const validMultipleProductPayload = {
@@ -15,7 +49,6 @@ beforeEach((done) => {
       payload: JSON.stringify(validMultipleProductPayload),
     };
     server.inject(request, (response) => {
-      console.log(response.result.action);
       done();
     });
   });
@@ -31,11 +64,10 @@ describe('Tests for fetching product details sorted by category from internal DB
   test('Test for successful response with existing product category in the DB', (done) => {
     const request = {
       method: 'GET',
-      url: '/api/v1/products/category/TVs',
+      url: '/api/v1/products/category/TV & Home Theater',
     };
     server.inject(request, (response) => {
-      console.log(response.result);
-      expect(response).toBe(false);
+      expect(response.result.data).toEqual(productsInDb);
       done();
     });
   });
@@ -45,8 +77,10 @@ describe('Tests for fetching product details sorted by category from internal DB
       url: '/api/v1/products/category/Gifts',
     };
     server.inject(request, (response) => {
-      console.log(response.result);
-      expect(response).toBe(false);
+      expect(response.result).toEqual({
+        statusCode: 404,
+        error: 'Product/s not found',
+      });
       done();
     });
   });
