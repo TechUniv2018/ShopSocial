35478adb0830af46913ae908aa139534e1654beb
balodi <balodi@gmail.com>
2018-02-22 21:25:39 +0530

    migration-admindetails table - modified test description

--- __tests__/models/AdminDetails.test.js
+++ __tests__/models/AdminDetails.test.js
@@ -4,35 +4,35 @@ afterEach(() => Models.AdminDetails.destroy({ truncate: true }));
 afterAll(() => Models.close());
 
 describe('Testing the AdminDetails model', () => {
-  it('testing if table returns the inserted sample object', (done) => {
-    const sampleUser = {
+  it('testing if admin table returns the inserted record', (done) => {
+    const sampleAdmin = {
       name: 'Admin',
       email: 'Admin@gmail.com',
-      password: '#2323atfagd',
+      password: '#2323atfagD',
     };
 
-    Models.AdminDetails.create(sampleUser).then(() =>
+    Models.AdminDetails.create(sampleAdmin).then(() =>
       Models.AdminDetails.find().then((result) => {
-        expect(result.email).toBe(sampleUser.email);
-        expect(result.name).toBe(sampleUser.name);
-        expect(result.password).toBe(sampleUser.password);
+        expect(result.email).toBe(sampleAdmin.email);
+        expect(result.name).toBe(sampleAdmin.name);
+        expect(result.password).toBe(sampleAdmin.password);
         done();
       })).catch(err => console.log(err));
   });
-  it('testing with 2 sample valid Admin objects', (done) => {
-    const sampleUser = {
+  it('testing if multiple records can be inserted into admin table', (done) => {
+    const sampleAdmin1 = {
       name: 'Allen',
       email: 'abc@gmail.com',
-      password: '#2323atfagd',
+      password: '#2323atfagD',
     };
 
-    const sampleUser2 = {
+    const sampleAdmin2 = {
       name: 'Pinnochio',
       email: 'def@gmail.com',
-      password: '#basad65',
+      password: '#Basad65',
     };
 
-    Models.AdminDetails.bulkCreate([sampleUser, sampleUser2]).then(() => {
+    Models.AdminDetails.bulkCreate([sampleAdmin1, sampleAdmin2]).then(() => {
       Models.AdminDetails.findAll({ attributes: ['name'] }).then((result) => {
         expect(result.sort().map(a => a.name)).toEqual(['Allen', 'Pinnochio'].sort());
         done();
