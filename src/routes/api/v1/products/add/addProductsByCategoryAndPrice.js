7b6212b97273a6744111e0c5d1bb2533fbb13438
maheshhp <hpmahesh73@gmail.com>
2018-02-22 19:11:35 +0530

    handler - Converted request to JSON string and parsed it before sending it external fetch API

--- src/routes/api/v1/products/add/addProductsByCategoryAndPrice.js
+++ src/routes/api/v1/products/add/addProductsByCategoryAndPrice.js
@@ -6,29 +6,29 @@ module.exports = [
     method: 'POST',
     path: '/api/v1/products/add/',
     handler: (request, response) => {
-      getProductsByCategoryAndPrice(request.payload).then((remoteFetchResponse) => {
-        addProductsToDatabase(remoteFetchResponse.data).then((dbInsertResponse) => {
-          console.log(dbInsertResponse);
-          let addedCount = 0;
-          let updatedCount = 0;
-          dbInsertResponse.forEach((insertedValue) => {
-            if (insertedValue) {
-              addedCount += 1;
-            } else {
-              updatedCount += 1;
-            }
+      getProductsByCategoryAndPrice(JSON.parse(JSON.stringify(request.payload)))
+        .then((remoteFetchResponse) => {
+          addProductsToDatabase(remoteFetchResponse.data).then((dbInsertResponse) => {
+            let addedCount = 0;
+            let updatedCount = 0;
+            dbInsertResponse.forEach((insertedValue) => {
+              if (insertedValue) {
+                addedCount += 1;
+              } else {
+                updatedCount += 1;
+              }
+            });
+            response({
+              statusCode: 201,
+              action: `${addedCount} added, ${updatedCount} updated`,
+            });
           });
+        }).catch(() => {
           response({
-            statusCode: 201,
-            action: `${addedCount} added, ${updatedCount} updated`,
+            statusCode: 404,
+            action: 'No product added',
           });
         });
-      }).catch(() => {
-        response({
-          statusCode: 404,
-          action: 'No product added',
-        });
-      });
     },
   },
 ];
