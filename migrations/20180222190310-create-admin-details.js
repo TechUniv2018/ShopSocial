e282e226f5bbcd44465ddec33fd7ff9df7b6e92c
balodi <balodi@gmail.com>
2018-02-22 18:47:23 +0530

    migration- added migration for admin-details table

---
+++ migrations/20180222190310-create-admin-details.js
@@ -0,0 +1,32 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => queryInterface.createTable('AdminDetails', {
+    id: {
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+      type: Sequelize.INTEGER,
+    },
+    password: {
+      allowNull: false,
+      type: Sequelize.STRING,
+    },
+    name: {
+      allowNull: false,
+      type: Sequelize.STRING,
+    },
+    email: {
+      allowNull: false,
+      unique: true,
+      type: Sequelize.STRING,
+    },
+    createdAt: {
+      allowNull: false,
+      type: Sequelize.DATE,
+    },
+    updatedAt: {
+      allowNull: false,
+      type: Sequelize.DATE,
+    },
+  }),
+  down: (queryInterface, Sequelize) => queryInterface.dropTable('AdminDetails'),
+};
