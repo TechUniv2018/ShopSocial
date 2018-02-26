49abb397b0b0d478212a3bda7d8e621f0e1de1ee
balodi <balodi@gmail.com>
2018-02-26 18:18:10 +0530

    admin login -testcase passed for successful admin login - returns statusCode 200

---
+++ src/handlers/adminLoginHandler.js
@@ -0,0 +1,35 @@
+const findAdmin = require('../routes/admin/helper/findAdmin');
+const JWT = require('jsonwebtoken');
+const secret = require('../routes/admin/helper/secretKey');
+
+module.exports = (request, response) => {
+  findAdmin(request.payload.email, request.payload.password).then((status) => {
+    if ((status === 'valid')) {
+      const obj = { email: request.payload.email };
+      const token = JWT.sign(obj, secret);
+      const cookieOptions = {
+        ttl: 365 * 24 * 60 * 60 * 1000,
+        encoding: 'none',
+        isSecure: false,
+        isHttpOnly: true,
+        clearInvalid: false,
+        strictHeader: true,
+        path: '/',
+      };
+      response({
+        message: 'Admin Verified ',
+        statusCode: 200,
+      }).header('Authorization', token).state('token', token, cookieOptions);
+    } else if (status === 'invalid') {
+      response({
+        message: 'invalid credentials',
+        statusCode: 401,
+      });
+    } else {
+      response({
+        message: 'server error',
+        statusCode: 503,
+      });
+    }
+  });
+};
---
+++ src/tests/adminLogin/adminLogin.test.js
@@ -0,0 +1,34 @@
+const Server = require('../../server');
+const Models = require('../../../models');
+const passwordHash = require('password-hash');
+
+describe('Test server for POST /admin/login: ', () => {
+  beforeAll((done) => {
+    Models.AdminDetails.create({
+      email: 'admin@gmail.com',
+      password: passwordHash.generate('Admin@1234'),
+      name: 'Admin',
+    }).then(() => {
+      done();
+    });
+  });
+  afterAll((done) => {
+    Models.AdminDetails.destroy({ truncate: true }).then(() => {
+      done();
+    });
+  });
+  test('Should return statusCode 200: Successful admin login ', (done) => {
+    const options = {
+      url: '/admin/login',
+      method: 'POST',
+      payload: {
+        email: 'admin@gmail.com',
+        password: 'Admin@1234',
+      },
+    };
+    Server.inject(options, (response) => {
+      expect(response.result.statusCode).toBe(200);
+      done();
+    });
+  });
+});
