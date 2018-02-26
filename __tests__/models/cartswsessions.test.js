667fdd24408b71c0afd98183ac8f45813c3bd622
Aakash Verma <aku06_ran@yahoo.co.in>
2018-02-26 12:03:05 +0530

    CartWSessions - Added test for null sessionID

--- __tests__/models/cartswsessions.test.js
+++ __tests__/models/cartswsessions.test.js
@@ -4,12 +4,21 @@ afterEach(() => Models.CartsWSessions.destroy({ truncate: true }));
 afterAll(() => Models.close());
 
 describe('Testing the  CartsWSessions table:', () => {
-  it('Testing an object with a valid session ID', (done) => {
+  it('Testing an object with a valid sessionID', (done) => {
     Models.CartsWSessions.create({
       sessionID: '123',
     }).then(() => {
       Models.CartsWSessions.findOne().then((result) => {
         expect(result.sessionID).toBe('123');
+        done();
+      });
+    });
+  });
+  it('Testing an object with a null sessionID', (done) => {
+    Models.CartsWSessions.create({
+    }).then(() => {
+      Models.CartsWSessions.findOne().then((result) => {
+        expect(result.sessionID).toBe(null);
         done();
       });
     });
