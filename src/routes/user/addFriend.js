274e3ef4e096b94275fa99464f2a805e42c5a598
balodi <balodi@gmail.com>
2018-03-22 21:49:10 +0530

    modified addfriend.js file added response if user wants to add himself a friend

--- src/routes/user/addFriend.js
+++ src/routes/user/addFriend.js
@@ -9,20 +9,38 @@ const addFriend = {
   handler: (request, response) => {
     const { userId } = request.payload;
     const { friendsEmail } = request.payload;
-    Models.UserFriends.findOrCreate({ where: { userId, friendsEmail } })
-      .spread((user, created) => {
-        if (created) {
-          response({
-            message: 'friendAdded',
-            statusCode: 200,
-          });
-        } else {
-          response({
-            message: 'friendAlreadyAdded',
-            statusCode: 409,
-          });
-        }
-      });
+    Models.UserDetails.findOne({ where: { email: friendsEmail } }).then((result) => {
+      if (result !== null) {
+        Models.UserDetails.findOne({ where: { id: userId } }).then((UserDetails) => {
+          if (UserDetails.email === friendsEmail) {
+            response({
+              message: 'cannot Add Yourself As a friend',
+              statusCode: 422,
+            });
+          } else {
+            Models.UserFriends.findOrCreate({ where: { userId, friendsEmail } })
+              .spread((user, created) => {
+                if (created) {
+                  response({
+                    message: 'friend Added',
+                    statusCode: 201,
+                  });
+                } else {
+                  response({
+                    message: 'friend Already Added',
+                    statusCode: 409,
+                  });
+                }
+              });
+          }
+        });
+      } else {
+        response({
+          message: 'user whom you want to add as a friend doesnt exist',
+          statusCode: 403,
+        });
+      }
+    });
   },
 };
 module.exports = addFriend;
